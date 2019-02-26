27f6cec987777a58660cd7eb979674a2f04eb28d
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-02-26 10:46:07 -0500

    Creation with cookiecutter

---
+++ src/orion/algo/zero_order_momentum/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+:mod:`orion.algo.zero_order_momentum -- TODO
+=================================================
+
+.. module:: zero_order_momentum
+    :platform: Unix
+    :synopsis: TODO
+
+TODO: Write long description
+"""
+from ._version import get_versions
+
+VERSIONS = get_versions()
+del get_versions
+
+__descr__ = 'TODO'
+__version__ = VERSIONS['version']
+__license__ = 'BSD 3-Clause'
+__author__ = u'Thomas George, Xavier Bouthillier'
+__author_short__ = u'Thomas George, Xavier Bouthillier'
+__author_email__ = 'xavier.bouthillier@umontreal.ca'
+__copyright__ = u'2019, Thomas George, Xavier Bouthillier'
+__url__ = 'https://github.com/bouthilx/orion.algo.zero_order_momentum'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
